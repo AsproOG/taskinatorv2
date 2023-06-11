70a86a24b1de8fe8cc7c15e8cb749af93ccee3ee
AsproOG <116924836+AsproOG@users.noreply.github.com>
2023-06-11 19:52:54 -0400

    ab

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,12 +1,33 @@
-import react from "react"
+import React from "react";
+import './About.css';
 
 function About() {
-    return (   
-        <div className="About"> 
-           <h1>About</h1> 
-           <h2>Our vision is to be the place for companies to find the best of the best students through tasks listed by them. If a student can complete the rigorous tasks, they deserve to be hired. </h2>
-        </div>
-    )
+  return (
+    <div className="About">
+      <h1>About</h1>
+      <h2>  Our vision is to create an unparalleled platform where 
+        companies can effortlessly discover and connect with the most 
+        exceptional talent among students. We strive to bridge the gap 
+        between companies seeking top-tier individuals and students 
+        seeking meaningful opportunities to showcase their skills.    
+        </h2>
+        <h2>In today's competitive job market, it can be challenging for
+             companies to identify and recruit the best-suited candidates.
+              Likewise, students often struggle to gain visibility and secure
+               job offers that align with their capabilities. Our mission is to
+                solve this problem by providing a curated space that empowers both 
+                parties.</h2>
+                <h2>At our platform, companies can list tasks and projects that simulate real-world challenges they face within their industries. These tasks serve as a unique opportunity for students to demonstrate their abilities and problem-solving skills. By completing these rigorous tasks, students can showcase their competence and prove that they deserve to be hired.</h2>
+    <h2>We understand that hiring decisions should be based on more than just resumes or academic achievements. Our platform enables companies to witness firsthand the potential and capabilities of students through the completion of practical tasks. This allows companies to make informed hiring decisions and find candidates who can contribute effectively to their teams from day one. </h2>
+    <h2>For students, our platform provides a level playing field where their skills and achievements can shine. We believe that talent should not be limited by traditional barriers such as lack of work experience or institutional reputation. By participating in tasks listed by companies, students can demonstrate their true potential and be recognized for their abilities, regardless of their background. </h2>
+    <h2> We aim to create a community where students are motivated and empowered to constantly learn and improve. Our platform offers resources, mentorship opportunities, and a supportive network that fosters growth and development. We believe that learning is a lifelong journey, and we encourage students to continuously enhance their skills and stay up-to-date with industry trends.</h2>
+    <h2>Transparency and fairness are core values that guide our platform. We prioritize providing equal opportunities to all students, irrespective of their background, gender, or ethnicity. We are committed to eliminating bias and discrimination in the hiring process, ensuring that companies evaluate candidates solely based on their skills and merit.</h2>
+    <h2> Additionally, we strive to cultivate strong relationships with companies, educational institutions, and industry professionals. By collaborating with reputable organizations, we can expand our reach, offer valuable resources, and create a robust ecosystem that benefits all stakeholders.</h2>
+    <h2>Ultimately, our vision is to revolutionize the way companies find talent and how students are discovered for their exceptional abilities. We envision a future where companies effortlessly tap into a vast pool of talented students, and where students have equal access to opportunities that recognize and reward their skills. Together, we can transform the hiring landscape and empower the next generation of professionals. </h2>
+
+
+    </div>
+  );
 }
 
 export default About;
